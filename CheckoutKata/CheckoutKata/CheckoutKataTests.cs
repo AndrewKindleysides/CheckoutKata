0ac991bb0352ddf43d82eccff44ac3987707c415
AndrewKindleysides <andrew.kindleysides@advancedcomputersoftware.com>
2016-04-17 16:16:45 +0100

    added test for scanning items

--- CheckoutKata/CheckoutKata/CheckoutKataTests.cs
+++ CheckoutKata/CheckoutKata/CheckoutKataTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace CheckoutKata
 {
@@ -7,7 +8,21 @@ namespace CheckoutKata
         [Fact]
         public void Checkout_can_scan_an_item()
         {
-            
+            var checkout = new Checkout();
+            checkout.Scan("A");
+            Assert.Equal(1, checkout.ItemsInBasket);
         }
+    }
+
+    public class Checkout
+    {
+        public void Scan(string s)
+        {
+            var basket = new List<string>();
+            basket.Add(s);
+            ItemsInBasket++;
+        }
+
+        public int ItemsInBasket { get; set; }
     }
 }
