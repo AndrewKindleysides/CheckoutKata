b239f4638cb7e6e14b73ac72aa2636feab58f5f9
AndrewKindleysides <andrew.kindleysides@advancedcomputersoftware.com>
2016-04-17 16:10:49 +0100

    Added inital project

---
+++ CheckoutKata/CheckoutKata/CheckoutKataTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace CheckoutKata
+{
+    public class CheckoutKataTests
+    {
+        [Fact]
+        public void Checkout_can_scan_an_item()
+        {
+            
+        }
+    }
+}
