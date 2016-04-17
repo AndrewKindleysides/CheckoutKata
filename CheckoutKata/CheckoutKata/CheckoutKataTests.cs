8ce91f060f693d8c11689f8b8a7e2313f990cb11
AndrewKindleysides <andrew.kindleysides@advancedcomputersoftware.com>
2016-04-17 17:55:49 +0100

    added failing test for multiple discounts of the same item and singular items

--- CheckoutKata/CheckoutKata/CheckoutKataTests.cs
+++ CheckoutKata/CheckoutKata/CheckoutKataTests.cs
@@ -69,17 +69,25 @@ namespace CheckoutKata
             Assert.Equal(255, _checkout.GetTotalPrice());
         }
 
-        [Fact]
-        public void Checkout_calculates_total_price_with_multiple_discounts_of_the_same_item()
+        [Fact(Skip = "initial test for multiple discounts of the same item and singular items")]
+        public void Checkout_calculates_total_price_with_multiple_discounts_of_the_same_item_and_singular_items()
         {
             _checkout.Scan("A");
             _checkout.Scan("A");
             _checkout.Scan("A");
+            
             _checkout.Scan("A");
             _checkout.Scan("A");
             _checkout.Scan("A");
             
-            Assert.Equal(260, _checkout.GetTotalPrice());
+            _checkout.Scan("B");
+            _checkout.Scan("B");
+            
+            _checkout.Scan("B");
+            _checkout.Scan("B");
+
+            _checkout.Scan("B");
+            Assert.Equal(390, _checkout.GetTotalPrice());
         }
 
     }
